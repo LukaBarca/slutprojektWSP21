4a750a252f4f2307b43b5cfbab8e8aac8bbe5eb8
LukaBarca <luka.rajacic@elev.ga.ntig.se>
2021-03-18 16:00:36 +0100

    d

--- app.rb
+++ app.rb
@@ -3,6 +3,38 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
+get("/") do
+    slim(:start)
+end
+
+get("/albums") do
+    db = SQLite3::Database.new("db/chinook-crud.db")
+    db.results_as_harsh = true
+    result = db.execute("SELECT * FROM albums")
+    p result
+    slim(:"albums/index",locals:{albums:result})
+end
+
+get("/albums/new") do
+    slim(:"albums/new")
+
+end
+
+end
+
+get("/albums/:id") do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/chinook-crud.db")
+    db.results_as_harsh = true
+    result = db.execute("SELECT * FROM albums WHERE AlbumId = ?",id).first
+    ...
+    slim(:"albums/show", locals:{result:result})
+end
+
+
+
+
+
 
 get("/") do
     slim(:register)
