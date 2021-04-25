63ca22ec902411da4238435ce22af74d103d2963
LukaBarca <luka.rajacic@elev.ga.ntig.se>
2021-04-25 19:08:08 +0200

    h
    h

--- app.rb
+++ app.rb
@@ -3,59 +3,127 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
-get("/") do
-    slim(:start)
+enable :sessions
+
+
+
+get('/') do
+  slim(:register)
 end
 
-get("/albums") do
-    db = SQLite3::Database.new("db/chinook-crud.db")
-    db.results_as_harsh = true
-    result = db.execute("SELECT * FROM albums")
-    p result
-    slim(:"albums/index",locals:{albums:result})
+get('/showlogin') do
+  slim(:login)
 end
 
-get("/albums/new") do
-    slim(:"albums/new")
+post("/login") do
+  username = params[:username]
+  password = params[:password]
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+  pwdigest = result["pwdigest"]
+  id = result["id"]
 
+  if BCrypt::Password.new(pwdigest) == password
+    session[:id] = id
+    redirect("/todos")
+  else
+    "FEL LÖSENORD!"
+  end
 end
 
-end
+get("/todos") do
+  id = session[:id].to_i
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM todos WHERE user_id = ?",id)
+  p "Alla todos från result #{result}"
+  slim(:"todos/hej",locals:{todos:result})
 
-get("/albums/:id") do
-    id = params[:id].to_i
-    db = SQLite3::Database.new("db/chinook-crud.db")
-    db.results_as_harsh = true
-    result = db.execute("SELECT * FROM albums WHERE AlbumId = ?",id).first
-    ...
-    slim(:"albums/show", locals:{result:result})
-end
-
-
-
-
-
-
-get("/") do
-    slim(:register)
 end
 
 post("/users/new") do
-    username = params[:username]
-    password = params[:password]
-    password_confirm = params[:password_confirm]
+  username = params[:username]
+  password_confirm = params[:password_confirm]
+  password = params[:password]
 
-    if (password == password_confirm)
-        #Lägg till användare
-        password_digest = Bcrypt::Password.create(password)
-        db = SQlite3::Database.new('db/')...
-    
-    
-    
-    else
-        #Felhantering
-        "Lösenorden matchade inte"
-    
+  if (password == password_confirm)
+    #Lägg till andvändare
+    password_digest = BCrypt::Password.create(password)
+    db = SQLite3::Database.new("db/todo2021.db")
+    db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
+    redirect("/")
+
+  else
+    #Felhantering
+    "Lösenodern matchade inte!"
+  end
+end
+
+
+get('/')  do
+  slim(:start)
+end 
+
+get('/albums') do
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM albums")
+  p result
+  slim(:"albums/index",locals:{albums:result})
+end
+
+get("/albums/new") do 
+  slim(:"albums/new")
+end
+
+
+post("/albums/new") do
+  title = params[:title]
+  artist_id = params[:artist_id].to_i
+  p "Vi fick in datan #{title} och #{artist_id}"
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.execute("INSERT INTO albums (Title, ArtistId) VALUES (?,?)",title, artist_id)
+  redirect("/albums")
+end
+
+post("/albums/:id/delete") do
+  id = params[:id].to_i
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.execute("DELETE FROM albums WHERE AlbumId = ?", id)
+  redirect("/albums")
+end
+
+post("/albums/:id/update") do
+  id = params[:id].to_i
+  title = params[:title]
+  artist_id = params[:artistId].to_i
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.execute("UPDATE albums SET Title=?,ArtistId=? WHERE AlbumId = ?",title,artist_id,id)
+  redirect("/albums")
 
 
 end
+
+
+get("/albums/:id/edit") do
+  id = params[:id].to_i
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM albums WHERE AlbumId = ?", id).first
+  p "result är #{result}"
+  slim(:"/albums/edit",locals:{result:result})
+end
+
+
+get('/albums/:id') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new("db/todo2021.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM albums WHERE AlbumId = ?",id).first
+  result2 = db.execute("SELECT Name FROM artists WHERE ArtistId IN (SELECT ArtistId FROM Albums WHERE AlbumId = ?)",id).first
+  p "Resultat2 är: #{result2}"
+  slim(:"/albums/show",locals:{result:result,result2:result2})
+end
+
+
--- projektplan.md
+++ projektplan.md
@@ -1,6 +1,6 @@
 # Projektplan
 ## 1. Projektbeskrivning (Beskriv vad sidan ska kunna göra).
-Sidan ska vara ett sätt för användare att spara sina favorit låtar och artister i olika spellistor. Man ska kunna ta bort sina favorit låtar och även få olika rekommendationer på senaste releases. För att ha den här möjligheten, måste man skapa ett konto. Därefter kommer man få logga in genom ett inloggnings system som ska använda sig av validering för att skapa extra säkerhet i systemet.
+Sidan ska vara ett sätt för användare att samla sina favorit låtar och artister i olika spellistor. Man ska kunna lägga till och ta bort olika albums. Jag kommer även att implementera ett inloggnings system som ska använda sig av validering för att skapa extra säkerhet i systemet.
 ## 2. Vyer (visa bildskisser på dina sidor).
 Bilderna är i misc-mappen
 ## 3. Databas med ER-diagram (Bild på ER-diagram).
